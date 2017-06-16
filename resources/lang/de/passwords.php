9608eef9585bb3c9adeca85051c3f4e00a6333ec
ilu33 <ilu33@users.noreply.github.com>
2017-06-16 22:50:37 +0200

    Starting german translation

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passwörter müssen zumindest sechs Zeichen lang sein und bestätigt werden.',
+    'user'     => 'Wir können keinen Benutzer mit dieser E-mail-Addresse finden.',
+    'token'    => 'Dieses Token zum Zurücksetzen des Passworts ist ungültig.',
+    'sent'     => 'Wir haben einen Link zum Zurücksetzen des Passworts versandt!',
+    'reset'    => 'Ihr Passwort wurde zurückgesetzt!',
+
+];
